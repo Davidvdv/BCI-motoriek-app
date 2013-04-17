dc246fc6b378953488ca21faaf60bc22f8844cbc
David <d.vandevondervoort@gmail.com>
2013-04-17 09:10:28 +0200

    Init experiment branch

---
+++ MotoriekApp/ExercisesTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  ExercisesTableViewController.h
+//  MotoriekApp
+//
+//  Created by David van de Vondervoort on 16-04-13.
+//  Copyright (c) 2013 David van de Vondervoort. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ExercisesTableViewController : UITableViewController
+
+@end
